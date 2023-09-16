2f87d3784df1f1a99bdd78f7b7dbc0a1fb50a839
Logan Parke <logan.c.parke@gmail.com>
2023-09-15 18:47:09 -0600

    d

--- src/pages/floral/Floral.js
+++ src/pages/floral/Floral.js
@@ -3,15 +3,44 @@ import "./floral.css";
 
 function Floral() {
   return (
-    <div>
+    <div className="tablesPageWrap">
       <Link className="noUnderline" to="/floral/roses">
         <h3 className="productLink" id="roses">
           <p>Roses</p>
         </h3>
       </Link>
-      <Link className="noUnderline" to="/floral/hydrangeas">
+      <Link className="noUnderline tableLink" to="/floral/hydrangeas">
         <h3 className="productLink" id="hydrangeas">
           <p>Hydrangeas</p>
+        </h3>
+      </Link>
+    </div>
+  );
+  return (
+    <div className="tablesPageWrap">
+      <Link className="noUnderline tableLink" to="/tables/farmHouse">
+        <h3 className="productLink" id="chalk1">
+          <p>Floral <br></br>Backdrop</p>
+        </h3>
+      </Link>
+      <Link className="noUnderline tableLink" to="/tables/farmHouse">
+        <h3 className="productLink" id="chalk1">
+        <p>Farm House <br></br> Table</p>
+        </h3>
+      </Link>
+      <Link className="noUnderline tableLink" to="/tables/farmHouse">
+        <h3 className="productLink" id="chalk1">
+        <p>Farm House <br></br> Table</p>
+        </h3>
+      </Link>
+      <Link className="noUnderline tableLink" to="/tables/farmHouse">
+        <h3 className="productLink" id="chalk1">
+        <p>Farm House <br></br> Table</p>
+        </h3>
+      </Link>
+      <Link className="noUnderline tableLink" to="/tables/farmHouse">
+        <h3 className="productLink" id="chalk1">
+        <p>Farm House <br></br> Table</p>
         </h3>
       </Link>
     </div>
